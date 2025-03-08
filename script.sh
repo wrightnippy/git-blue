7dd77419d216d336adbf6f5a99adce65868fff31
wrightnippy <wrightgeegbe22@gmail.com>
2025-03-08 10:22:25 -0600

    script.sh

---
+++ script.sh
@@ -0,0 +1,10 @@
+!# bin/bash
+
+sudo yum update -y
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+wget https://github.com/startbootstrap/startbootstrap-creative/archive/gh-pages.zip
+unzip gh-pages-zip
+cp -r startbootstrap-creative-gh pages/* /var/www/html/
+echo "tom"
