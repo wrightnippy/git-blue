5dcc3d3d54a522d4d05813c81feabc8d8c30d26e
wrightnippy <wrightgeegbe22@gmail.com>
2025-03-10 01:53:07 -0500

    file.txt

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "charles" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "main"
+    Name = "charles"
   }
 }
